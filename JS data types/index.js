8316a873401f8e90086c0d5c0d447833d838470f
Prima Wirawan <primailkom@gmail.com>
2023-09-18 09:50:00 +0700

    Add contoh JS data types

---
+++ JS data types/index.js
@@ -0,0 +1,12 @@
+let length = 16;
+
+let lastName = "Johnson";
+
+const x = {
+  firstName: "John",
+  lastName: "Doe",
+};
+
+console.log(typeof length); // Outputs: "number"
+console.log(typeof lastName); // Outputs: "string"
+console.log(typeof x); // Outputs: "object"
