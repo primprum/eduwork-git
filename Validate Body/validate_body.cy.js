90d0bf83140809b6ce5fbd37f730a7b9b9c3fd41
Prima Wirawan <primailkom@gmail.com>
2023-10-23 18:02:36 +0700

    Assert content on JSON response body

---
+++ Validate Body/validate_body.cy.js
@@ -0,0 +1,15 @@
+describe("PokeAPI Tests", () => {
+  it("should assert the JSON response body of a Pokemon", () => {
+    // Make an HTTP GET request to the PokeAPI
+    cy.request("https://pokeapi.co/api/v2/pokemon/25").then((response) => {
+      // Assert on the status code of the response
+      expect(response.status).to.equal(200);
+
+      // Assert on the JSON response body
+      expect(response.body).to.have.property("name", "pikachu");
+      expect(response.body).to.have.property("id", 25);
+      expect(response.body).to.have.property("height", 4);
+      expect(response.body).to.have.property("weight", 60);
+    });
+  });
+});
